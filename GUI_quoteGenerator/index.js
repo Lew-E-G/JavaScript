de4fd39b97ffc450a95139082745f4be50147eca
Lewis <luigizzle@gmail.com>
2024-10-21 17:07:00 +0100

    quote gen function complete CSS and comments to be added

--- GUI_quoteGenerator/index.js
+++ GUI_quoteGenerator/index.js
@@ -13,10 +13,28 @@ const quotes = [
 "Success is how high you bounce when you hit bottom. - George S. Patton",
 "What lies behind us and what lies before us are tiny matters compared to what lies within us. - Ralph Waldo Emerson",
 "Act as if what you do makes a difference. It does. - William James",
-"Keep your face always toward the sunshine—and shadows will fall behind you. - Walt Whitman",
+"Keep your face always toward the sunshine — and shadows will fall behind you. - Walt Whitman",
 "The best way to predict the future is to create it. - Peter Drucker",
 "Success usually comes to those who are too busy to be looking for it. - Henry David Thoreau",
 "It always seems impossible until it's done. - Nelson Mandela",
 "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
 "Dream big and dare to fail. - Norman Vaughan"
 ]
+
+const usedIndexes = new Set()
+
+function generateQuote() {
+    if (usedIndexes.size >= quotes.length) {
+        usedIndexes.clear()
+    }
+    while (true) {
+        const randomIdx = Math.floor(Math.random() * quotes.length);
+        if (usedIndexes.has(randomIdx)) continue
+        const quote = quotes[randomIdx];
+        const quoteElement = document.getElementById("quote");
+        quoteElement.innerHTML = quote;
+        usedIndexes.add(randomIdx)
+        break
+    }
+
+}
