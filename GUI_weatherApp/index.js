d0f43ab54dd760cb0a6eeb06369e213343923e9d
Lewis <luigizzle@gmail.com>
2024-11-12 14:48:08 +0100

    fix to call, testing of API key

--- GUI_weatherApp/index.js
+++ GUI_weatherApp/index.js
@@ -1,9 +1,10 @@
-const apiKey = null
+//const apiKey = require('./keys')
+const apiKey = "a479a2b4e8243ea63fef94a3c039ec21"
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric"
 
 async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    var data = await response.json
+    var data = await response.json();
 
     console.log(data)
 }
