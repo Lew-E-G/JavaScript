24c889700f794def8f82b616f1e691d787cca495
Lewis <luigizzle@gmail.com>
2024-11-13 16:22:32 +0000

    linking query data to weather widget

--- GUI_weatherApp/index.js
+++ GUI_weatherApp/index.js
@@ -1,12 +1,17 @@
-//const apiKey = require('./keys')
-const apiKey = "a479a2b4e8243ea63fef94a3c039ec21"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric"
+//import { apiKey } from "./keys";
+const apiKey = "a479a2b4e8243ea63fef94a3c039ec21";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=york&units=metric"
 
 async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`)
     var data = await response.json();
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
 }
 
 checkWeather()
