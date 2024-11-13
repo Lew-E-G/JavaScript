179411438bec20edc6170a8290c3bcabc2904ed6
Lewis <luigizzle@gmail.com>
2024-11-13 16:41:44 +0000

    adding seach box inout to the API call

--- GUI_weatherApp/index.js
+++ GUI_weatherApp/index.js
@@ -1,17 +1,19 @@
 //import { apiKey } from "./keys";
 const apiKey = "a479a2b4e8243ea63fef94a3c039ec21";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=york&units=metric"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+const searchBox = document.querySelector(".search input")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await response.json();
 
     console.log(data)
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp)  + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
 
 checkWeather()
